b76a3d3ed5f97da2d98238422118a86985ce7bfa
Stefan Janssen <StefanJanssen95@users.noreply.github.com>
2019-08-27 11:28:32 +0200

    Make dynamic

---
+++ main.js
@@ -0,0 +1,60 @@
+(() => {
+	init();
+
+	function init() {
+		const eatingPeriods = [
+			TimePeriod(Time(18, 00), Time(19, 00)),
+			TimePeriod(Time(12, 00), Time(13, 00)),
+			TimePeriod(Time(07, 00), Time(09, 00))
+		];
+		const answerElement = document.getElementById('answer-text');
+		const cssClasses = { answerPositive: 'answer-text--positive', answerNegative: 'answer-text--negative' }
+
+		isEatingPeriod = false;
+		for (const period of eatingPeriods) {
+			if (period.isTimeWithinPeriod(new Date())) {
+				isEatingPeriod = true;
+				break;
+			}
+		}
+
+		answerElement.innerHTML = isEatingPeriod ? 'Ja' : 'Nee';
+		answerElement.classList.remove(cssClasses.answerPositive);
+		answerElement.classList.remove(cssClasses.answerNegative);
+		answerElement.classList.add(isEatingPeriod ? cssClasses.answerPositive : cssClasses.answerNegative);
+	}
+
+	function TimePeriod(time1, time2) {
+
+		const period = {
+			time1: time1,
+			time2: time2
+		}
+		return {
+			isTimeWithinPeriod: function (datetime) {
+				const currentTime = Time(datetime.getHours(), curM = datetime.getMinutes());
+				return (currentTime.greaterThanOrEqualTo(time1) && !currentTime.greaterThanOrEqualTo(time2))
+			}
+		}
+	}
+
+	function Time(hours, minutes) {
+		const timeObj = {
+			hours: hours,
+			minutes: minutes,
+		}
+
+		function getHours() { return timeObj.hours }
+		function getMinutes() { return timeObj.minutes }
+		function greaterThanOrEqualTo(otherTime) {
+			return otherTime.getHours() > getHours() || (otherTime.getHours() == getHours() && otherTime.getMinutes() >= getMinutes());
+		}
+
+		return {
+			getHours,
+			getMinutes,
+			greaterThanOrEqualTo
+		};
+	}
+})();
+
